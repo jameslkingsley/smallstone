a84f6394858f45926e48945d6f0759ff020dd92e
James Kingsley <jameslkingsley@outlook.com>
2017-11-05 13:50:58 +0000

    Work in progress

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,9 +2,10 @@
 <html lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="utf-8">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="google-site-verification" content="6D7p5YzEc2S9HzbwH0gnI7xKTuvhn6WRTRc9YKcFTR0">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
